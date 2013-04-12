c3ac9e7beef96df172e6155fa4a9af01b7ee84d2
Damon Snyder <drsnyder@gmail.com>
2013-04-12 09:18:41 -0700

    adding bisect

--- src/think_stats/distributions.clj
+++ src/think_stats/distributions.clj
@@ -5,45 +5,12 @@
               [stats :as stats]
               [homeless :as h])))
 
-(defn- vswap! 
-  [v a b]
-  (let [t (nth v b)]
-    (assoc! v b (nth v a))
-    (assoc! v a t)))
-
-(defn- partition [l left right idx]
-  (let [pivot-value (nth l idx)
-        store-index (atom left)]
-    (vswap! l idx right)
-    (loop [i left]
-      (when (< (nth l i) pivot-value)
-        (vswap! l @store-index i)
-        (swap! store-index inc))
-      (when (< i right)
-        (recur (inc i))))
-    (vswap! l right @store-index) 
-    @store-index))
-
-; see http://en.wikipedia.org/wiki/Selection_algorithm
-(defn- select 
-  [l left right k]
-  (if (= left right)
-    (nth l left)
-    ; this is an arbitrary selection of the pivot index that may not serve use
-    ; well in some cases
-    (let [new-index (partition l left right (int (/ (+ left right) 2)))
-          dist (inc (- new-index left))]
-      (cond 
-        (= dist k) (nth l new-index)
-        (= new-index k) (nth l new-index) ; same as above
-        (< k dist) (select l left (dec new-index) k)
-        :else (select l (inc new-index) right (- k dist))))))
 
 (defn percentile
   [s k]
   (let [scaled-k (* (count s) (/ k 100))
         s (vec s)] ; force realization if lazy
-    (select (transient s) 0 (dec (count s)) scaled-k)))
+    (h/select (transient s) 0 (dec (count s)) scaled-k)))
 
 (defn percentile-rank
   [scores yours]
--- src/think_stats/homeless.clj
+++ src/think_stats/homeless.clj
@@ -2,15 +2,74 @@
   (:require (think-stats [stats :as stats]
                          [util :as util])))
 
-(defn select*
-  [s fields] 
-   (for [r s] 
-     (select-keys r fields)))
-
 
 (defn map-map
   [f m]
   (into {} (for [[k v] m] [k (f v)])))
+
+(defn- vswap! 
+  [v a b]
+  (let [t (nth v b)]
+    (assoc! v b (nth v a))
+    (assoc! v a t)))
+
+(defn- partition-by-idx [l left right idx]
+  (let [pivot-value (nth l idx)
+        store-index (atom left)]
+    (vswap! l idx right)
+    (loop [i left]
+      (when (< (nth l i) pivot-value)
+        (vswap! l @store-index i)
+        (swap! store-index inc))
+      (when (< i right)
+        (recur (inc i))))
+    (vswap! l right @store-index) 
+    @store-index))
+
+; see http://en.wikipedia.org/wiki/Selection_algorithm
+(defn select 
+  [l left right k]
+  (if (= left right)
+    (nth l left)
+    ; this is an arbitrary selection of the pivot index that may not serve use
+    ; well in some cases
+    (let [new-index (partition-by-idx l left right (int (/ (+ left right) 2)))
+          dist (inc (- new-index left))]
+      (cond 
+        (= dist k) (nth l new-index)
+        (= new-index k) (nth l new-index) ; same as above
+        (< k dist) (select l left (dec new-index) k)
+        :else (select l (inc new-index) right (- k dist))))))
+
+(defmulti compute-bisection-ends (fn [dir s x low mid high] dir))
+
+(defmethod compute-bisection-ends :right 
+  ([dir s x low mid high]
+   (cond
+     (< x (nth s mid)) [low mid]
+     :else [(inc mid) high])))
+
+(defmethod compute-bisection-ends :left
+  ([dir s x low mid high]
+   (cond
+     (< (nth s mid) x) [(inc mid) high]
+     :else [low mid])))
+  
+(defmethod compute-bisection-ends :default
+  ([dir s x low mid high]
+   (compute-bisection-ends :right s x low mid high)))
+
+(defn bisect
+  [s x & [dir]] 
+  (loop [low 0 
+         high (count s)]
+    (if (>= low high)
+      low
+      (let [mid (quot (+ low high) 2)
+            [l h] (compute-bisection-ends dir s x low mid high)]
+        (prn (>= high 10) low mid high x)
+        (recur l h)))))
+    
 
 
 ;;;;;;;;;;;;;
