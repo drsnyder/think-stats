89b9c0f6ab1def84c0bc10730f6c351e0bff9d52
Damon Snyder <drsnyder@gmail.com>
2013-06-17 07:13:25 -0700

    adding raw data

--- plots/bakery.R
+++ plots/bakery.R
@@ -1,4 +1,4 @@
-# Rscript bakery.R bakery.csv
+# Rscript bakery.R bakery.csv bakery-raw.csv
 # Exercise 5.6
 # Example with n = 4
 # (def r (five/compare-baker-and-poincare 950 50 4 1000))
@@ -9,6 +9,17 @@ require(reshape2)
 
 args <- commandArgs(TRUE)
 breada <- read.csv(args[1], header=TRUE)
+rawbreada <- read.csv(args[2], header=TRUE)
+
+
+# generate the boxplot
+png("plots/bakery-boxplot.png")
+boxplot(rawbreada$baker, rawbreada$poincare, col=c("lightgreen", "lightblue"))
+axis(side=1, 1:2, labels=c("Baker", "Poincaré"), cex.axis=0.8)
+title("Baker vs Poincaré")
+dev.off()
+
+t.test(rawbreada$baker, rawbreada$poincare, alternative="less")
 
 ggplot(data=breada) +
     geom_line(aes(x=bweights,y=Baker,color=c("Baker"))) +
