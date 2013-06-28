568c7aa3558acc4fce82b1d86e7cf1f82bba7fb1
Damon Snyder <drsnyder@gmail.com>
2013-06-27 22:35:55 -0700

    Adding recipe.

---
+++ src/think_stats/recipe/pregnancy.clj
@@ -0,0 +1,8 @@
+(ns think-stats.recipe.pregnancy
+  (:require (think-stats [util :as util]
+                         [homeless :as h]
+                         [survey :as s]
+                         [pregnancy :as preg]
+                         [stats :as stats])))
+
+; FIXME: move think-stats.pregnancy here
