55cf268aaca70c9858a390b60f89a57648ae7764
Damon Snyder <drsnyder@gmail.com>
2013-08-29 17:37:54 -0700

    Minor updates to chapter seven.

--- src/think_stats/chapters/seven.clj
+++ src/think_stats/chapters/seven.clj
@@ -5,25 +5,33 @@
                          [random :as random])))
 
 (defn mean-difference
+  "Returns [meana meanb difference]"
+  [colla collb]
+  (let [meana (stats/mean colla)
+        meanb (stats/mean collb)]
+    [meana meanb (double (- meana meanb))]))
+
+
+(defn mean-difference-sim
   "Helper function for reproducing the data in section 7.1 using."
   [edist-a edist-b n]
   (let [pool (vec (concat edist-a edist-b))
         size-a (count edist-a)
         size-b (count edist-b)
-        mean-diff-edist (Math/abs (double (-
-                                           (stats/mean edist-a)
-                                           (stats/mean edist-b))))
-        a-minus-b (repeatedly n
+        [meana meanb mean-diff-edist] (mean-difference edist-a edist-b)
+        mean-diff-edist (Math/abs mean-diff-edist)
+        mean-delta-dist (repeatedly n
                               (fn []
                                 (let [sample-a (random/choice-seq size-a pool)
                                       sample-b (random/choice-seq size-b pool)
-                                      mean-diff-samples (double (-
-                                                                 (stats/mean sample-a)
-                                                                 (stats/mean sample-b)))]
+                                      [ma mb mean-diff-samples] (mean-difference sample-a sample-b)]
                                   mean-diff-samples)))
-        outside-mean-diff (filter #(>= (Math/abs %) mean-diff-edist) a-minus-b)
+        mean-of-means (stats/mean mean-delta-dist)
+        var-of-means (stats/variance mean-delta-dist)
+        j (prn (format "mean %2.3f var %2.3f of resampled deltas" mean-of-means var-of-means))
+        outside-mean-diff (filter #(>= (Math/abs %) mean-diff-edist) mean-delta-dist)
         outside (count outside-mean-diff)
-        cdf (cdf/cdff a-minus-b)
+        cdf (cdf/cdff mean-delta-dist)
         left (cdf (* -1 mean-diff-edist))
         right (- 1 (cdf mean-diff-edist))
         p-value (+ left right)]
@@ -43,6 +51,6 @@
         other (if sample-size
                 (random/sample-seq sample-size other)
                 other)
-        stats (mean-difference first-babies other n)]
+        stats (mean-difference-sim first-babies other n)]
     stats))
 
