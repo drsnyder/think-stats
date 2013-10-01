243ea6ab9440406fef7a967ea28e1dc4c4d58b1a
Damon Snyder <drsnyder@gmail.com>
2013-09-30 17:13:31 -0700

    Simplify vote-count-sim.

--- src/think_stats/chapters/eight.clj
+++ src/think_stats/chapters/eight.clj
@@ -16,11 +16,10 @@
   (eight/vote-count-sim  {0 0.05 2 0.05 1 0.9} 1e6 1e6 10))
 
 (defn vote-count-sim
-  [prob-map a-votes b-votes iter]
-  (let [v (vote prob-map)
-        a (repeatedly iter #(h/sum (repeatedly a-votes v)))
-        b (repeatedly iter #(h/sum (repeatedly b-votes v)))]
-    (map vector a b)))
+  [prob-map votes iter]
+  (let [v (vote prob-map)]
+    (repeatedly iter #(h/sum (repeatedly votes v)))))
+
 
 ;(defn vote-sim
   ;[cdf count-a count-b iter]
